9884ff00cc2f3e8e5e8f97a053fadb7daacd52c6
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-01-26 08:08:11 +0530

    longest substring

---
+++ test1.js
@@ -0,0 +1,8 @@
+function common_substring(arr1){
+    var arr = arr1.concat().sort();
+    a1= arr[0], a2=[arr.length-1],L=a1.length,i=0;
+    while(i<L&&a1.charAt(i)===a2.charAt(i))
+    i++;
+    return a1.subString(0,i)
+}
+console.log(common_substring('ankur'));

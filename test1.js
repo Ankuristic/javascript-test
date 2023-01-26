375a9bdf70741c3a7211857f578ae3fce54f804d
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-01-26 08:34:39 +0530

    final

--- test1.js
+++ test1.js
@@ -8,7 +8,7 @@ function common_substring(arr1){
 console.log(common_substring('ankur'));
 
 //Implement a JS function that takes a list of integers and a target number as input, and returns a array of two integers that add up to the target number.
-function listofInteger(arr, target){
+function listofInteger(nums, target){
     for(let i = 0; i <= nums.length; i++){
         for(let j = 0; j <= nums.length; j++){
             if(nums[i] + nums[j] == target){
